0d6f53c2b2d2c31b878be9cce22bf2ea85ef0133
Leo <leonard.remus@uni-ulm.de>
2023-11-07 15:25:17 +0100

    added bash file

---
+++ CodeBERT/codesearch/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=leonard.remus@uni-ulm.de
+#SBATCH --output=testout.log
+#SBATCH --error=testerr.log
+
+source activate ../../../mixenv/bin/activate
+
+python runner.py "$@"
